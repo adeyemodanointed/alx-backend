a5427b5ba19e459b3cf6490b9e8ef9c74aaef154
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-04-19 21:03:52 +0000

    add documentation to methods

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -6,13 +6,16 @@ BaseCaching = __import__('base_caching').BaseCaching
 class BasicCache(BaseCaching):
     """Basic Cache Class"""
     def __init__(self):
+        """Iniitialization"""
         super().__init__()
 
     def put(self, key, item):
+        """Puts data into cache"""
         if (key is None or item is None):
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
+        """Gets from the cache"""
         return self.cache_data.get(key)
