473226ecf025af94ae20569cb19de536bb7dad96
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-04-26 00:05:35 +0000

    task 4

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -25,7 +25,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     """Index route"""
     return render_template("4-index.html")
