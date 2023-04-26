1db9870e17a00d61a82d12700556c64fc6fae1fb
adeyemodanointed <adeyemodanointed@yahoo.com>
2023-04-26 00:08:44 +0000

    add strict_slashes False

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -22,7 +22,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def index():
     """Index route"""
     return render_template("3-index.html")
